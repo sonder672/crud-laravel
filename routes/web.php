57d9975e7c924699d562c87bf973edfbef696f7a
Girondavid <juan8.ramirez@ucp.edu.co>
2022-04-24 16:09:39 -0500

    backend

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Postcontroller;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::resource('/admin/posts', PostController::class);
 
 Route::get('/', function () {
     return view('welcome');
